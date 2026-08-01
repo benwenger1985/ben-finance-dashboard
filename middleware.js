d1eab34aac47faf44c47b4cad69f63db0eb46a37
benwenger1985 <me@benwenger.com>
2026-08-01 21:27:52 +0300

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/:path*",
+};
+
+export function middleware(req) {
+  const basicAuth = req.headers.get("authorization");
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Auth Required", {
+    status: 401,
+    headers: { "WWW-Authenticate": 'Basic realm="לוח מעקב כלכלי"' },
+  });
+}

64daccb9f2099d96d343d25ac9621a62dc8ee4a7
benwenger1985 <me@benwenger.com>
2026-08-01 21:28:29 +0300

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <iframe
-      src="/dashboard.html"
-      title="לוח מעקב כלכלי"
-      style={{ position: "fixed", inset: 0, width: "100%", height: "100%", border: "none" }}
-    />
-  );
-}

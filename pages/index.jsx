538e470889786e6f32c9324ff49857c1c1500532
benwenger1985 <me@benwenger.com>
2026-08-01 21:29:17 +0300

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <iframe
+      src="/dashboard.html"
+      title="לוח מעקב כלכלי"
+      style={{ position: "fixed", inset: 0, width: "100%", height: "100%", border: "none" }}
+    />
+  );
+}
